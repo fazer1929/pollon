6b3ba1ee563463210588e43ad7d6db94dfa6eaf5
fazer1929 <abhishekagrawal8888@gmail.com>
2020-11-29 15:25:35 +0530

    Functionality Done

--- pollon/polls/models.py
+++ pollon/polls/models.py
@@ -1,12 +1,15 @@
 from django.db import models
-
+import datetime
+from . import helpers
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=300)
-    pub_date = models.DateTimeField('Date Published')
+    pub_date = models.DateTimeField('Date Published',default=datetime.datetime.now())
     open_for_all = models.BooleanField(default=True)
     def __str__(self):
         return self.question_text
+    def link(self):
+        return helpers.inttohex(self.id) 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question,on_delete=models.CASCADE)
--- pollon/polls/urls.py
+++ pollon/polls/urls.py
@@ -5,8 +5,10 @@ from . import views
 app_name = 'polls'
 urlpatterns=[
     path('',views.index,name="index"),
-    path('<int:question_id>/',views.details,name="details"),
-    path('results/<int:question_id>/',views.results,name="results"),
-    path('<int:question_id>/vote/',views.vote,name="vote"),
-    path('resultsdata/<str:question_id>',views.resultsData,name="resultsData")
+    path('<str:question_id>/',views.details,name="details"),
+    path('results/<str:question_id>/',views.results,name="results"),
+    path('<str:question_id>/vote/',views.vote,name="vote"),
+    path('resultsdata/<str:question_id>',views.resultsData,name="resultsData"),
+    path('add',views.addPoll,name="addPoll"),
+
 ]
